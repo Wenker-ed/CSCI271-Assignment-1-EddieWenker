2872be18c12225eb389782775fcdff3b28e20095
Eddie Wenker <ewenker2024@yahoo.com>
2026-01-21 23:42:20 -0500

    Add files via upload

---
+++ CSCI271_Assignment1_EddieWenker_W30713797.java
@@ -0,0 +1,21 @@
+/*
+*
+*
+*
+*
+*
+*
+*
+*
+*/
+
+//inputfile > program or something like this (redirection)
+//Don't need error checking
+public class CSCI271_Assignment1_EddieWenker_W30713797 {
+    public static void main(String[] args){
+
+
+
+
+    }
+}
